6a3d592bdbfadcddae3ab29e37c384fc7fb7940a
Denis Lessard <DenisLessard91@gmail.com>
2021-03-03 18:55:53 -0500

    API route start

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Recipe;
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware(['auth'])->group(function() {
+    Route::prefix('recipes')->group(function() {
+        // List of recipes
+        Route::get('/', function () {
+            return Recipe::all();
+        });
+    });
 });
